d4dd1f87bc3066fc77dca9fecfb376ae4d739d2e
Wei Chiao Chen <joess.cts02@g2.nctu.edu.tw>
2023-03-06 06:52:25 +0800

    fix:Input data missing

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,18 @@ import { createStore } from 'vuex'
 export const store = createStore({
   state () {
     return {
+      nameData: {
+        name: '',
+        lastName: ''
+      },
       pageData: {}
     }
   },
   mutations: {
+    setNameData (state, payload) {
+      state.nameData.name = payload.name
+      state.nameData.lastName = payload.lastName
+    },
     setPageData (state, payload) {
       state.pageData = { ...payload }
     }
