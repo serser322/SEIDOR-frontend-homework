55347315271c1f07e08a0f0a77100ecb8384e0f3
Wei Chiao Chen <joess.cts02@g2.nctu.edu.tw>
2023-03-05 22:13:27 +0800

    feat:Input validation

--- src/views/components/StepOne.vue
+++ src/views/components/StepOne.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import StepBar from '../../components/StepBar.vue'
 import BaseCard from '../../components/BaseCard.vue'
 import BaseButton from '../../components/BaseButton.vue'
@@ -19,32 +20,49 @@ const continueBtnData = {
 // Router
 const router = useRouter()
 const toStepTwo = function () {
-  router.push({ name: 'StepTwo' })
+  validate() && router.push({ name: 'StepTwo' })
 }
 
+const name = ref('')
+const lastName = ref('')
+
+const nameValidation = ref(true)
+const lastNameValidation = ref(true)
+
+const validate = function () {
+  nameValidation.value = !!name.value.trim()
+  lastNameValidation.value = !!lastName.value.trim()
+  return nameValidation.value && lastNameValidation.value
+}
 </script>
 
 <template>
   <div>
     <StepBar />
-    <BaseCard
-      :card-data="cardData"
-    >
+    <BaseCard :card-data="cardData">
       <template #card_content>
         <form>
-          <div class="form__input">
+          <div
+            class="form__input"
+            :class="{ 'required': !nameValidation }"
+          >
             <label for="name">Name</label>
             <input
               id="name"
+              v-model="name"
               type="text"
               placeholder="John"
               required
             >
           </div>
-          <div class="form__input">
+          <div
+            class="form__input"
+            :class="{ 'required': !lastNameValidation }"
+          >
             <label for="last_name">Last Name</label>
             <input
               id="last_name"
+              v-model="lastName"
               type="text"
               placeholder="Doe"
               requierd
@@ -77,13 +95,27 @@ form {
         color: red;
       }
     }
+  }
+}
 
-    input {
-      height: 30px;
-      border: 2px solid dodgerblue;
-      border-radius: 5px;
-      margin-top: 5px;
-    }
+input {
+  height: 30px;
+  border: 2px solid dodgerblue;
+  border-radius: 5px;
+  margin-top: 5px;
+}
+
+.required {
+  color: red;
+
+  input {
+    border: 1px solid red;
+  }
+
+  &:after {
+    content: 'Required';
+    font-size: 0.5rem;
+    margin: 0.2rem 0 0 0.1rem;
   }
 }
 
