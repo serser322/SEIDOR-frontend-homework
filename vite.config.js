dd6df89a31c9fb66f80fb2db1252b12b044ef20b
Wei Chiao Chen <joess.cts02@g2.nctu.edu.tw>
2023-03-06 07:21:05 +0800

    fix:Set base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    vue()
-  ]
+  base: '/SEIDOR-frontend-homework/',
+  plugins: [vue()]
 })
