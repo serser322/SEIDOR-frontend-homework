855c2ae41c373ea529dbd373aa51b179612031e4
Wei Chiao Chen <joess.cts02@g2.nctu.edu.tw>
2023-03-07 15:07:24 +0800

    fix:fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/VITE-page-test/', // Set for gh-pages
+  // base: '/SEIDOR-frontend-homework/', // Set for gh-pages
   plugins: [vue()]
 })
